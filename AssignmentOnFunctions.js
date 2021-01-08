1042f5c000f637d2d94858109b8539ae6d017e1e
Tharun038 <Tharun.matta@zemosolabs.com>
2021-01-08 15:19:34 +0530

    Functions using switch statement

--- AssignmentOnFunctions.js
+++ AssignmentOnFunctions.js
@@ -1,14 +1,19 @@
 var answer = prompt("You owe me $0. Please enter the amount you are paying");
 function change(amountPayed, origCost) {
-  var difference = origCost - amountPayed;
-  console.log(difference);
-  if (difference > 0) {
-    console.log("You gave too little. You still owe $" + difference);
-  } else if (difference < 0) {
-    console.log("You gave too much. Your change due is $" + difference);
-  } else {
-    console.log("You gave the exact amount");
-  }
-  return difference;
+    var difference = origCost - amountPayed;
+  switch(true){
+      case difference>0:
+        console.log("You gave too little. You still owe $" + difference);
+        break;
+    case difference==0:
+        console.log("You gave the exact amount");
+        break;
+    case difference<0:
+        console.log("You gave too much. Your change due is $" + difference);
+        break;
+    default:
+        console.log("pay check");
+    }
+return difference;
 }
-change(300, 400);
+change(100,500);
